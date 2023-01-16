862b5145466bebdaa5e62a3f8267d5e5a6653661
AbhijithSatheesan <122276182+AbhijithSatheesan@users.noreply.github.com>
2023-01-16 22:47:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+
+    var hover;
+   hover= document.getElementById("contact1");
+   hover.addEventListener("mouseenter",colorchange);
+
+   function colorchange(){
+hover.style.color="red"
+   }
+   function colorback(){
+    hover.style.color="rgb(131, 106, 106)"
+   }
+
+    var hover1;
+   hover1= document.getElementById("about1");
+   hover1.addEventListener("mouseenter",colorchange1);
+
+   function colorchange1(){
+hover1.style.color="red"
+   }
+   function colorback1(){
+    hover1.style.color="rgb(131, 106, 106)"
+   }
+
+   var hover2;
+   hover2= document.getElementById("support1");
+   hover2.addEventListener("mouseenter",colorchange2);
+
+   function colorchange2(){
+hover2.style.color="red"
+   }
+   function colorback2(){
+    hover2.style.color="rgb(131, 106, 106)"
+   }
+
+   var hover3;
+   hover3= document.getElementById("rides1");
+   hover3.addEventListener("mouseenter",colorchange3);
+
+   function colorchange3(){
+hover3.style.color="red"
+   }
+   function colorback3(){
+    hover3.style.color="rgb(131, 106, 106)"
+   }
